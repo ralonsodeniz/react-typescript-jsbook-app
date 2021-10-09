94a3dad64ac8a2c420d68364544ac53ade55689a
ralonsodeniz <ralonsodeniz@gmail.com>
2021-10-09 14:04:17 +0200

    feat(components): markdown-editor

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -1,15 +1,16 @@
-// import { useStartService } from '../hooks/use-start-service';
+import { useStartService } from '../hooks/use-start-service';
 import GlobalStyle from '../styles/GlobalStyle';
-// import CodeCell from '../components/code-cell';
+import CodeCell from '../components/code-cell';
 import TextEditor from '../components/text-editor';
 
 const App = () => {
-  // const service = useStartService();
+  const service = useStartService();
 
   return (
     <>
       <GlobalStyle />
       <TextEditor />
+      <CodeCell service={service} />
     </>
   );
 };
